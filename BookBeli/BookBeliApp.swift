487580c12f36aaf06bb07e006978feb27c87da50
Jiahui Chen <jchen23@stanford.edu>
2024-04-18 17:06:31 -0700

    Initial Commit

---
+++ BookBeli/BookBeliApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookBeliApp.swift
+//  BookBeli
+//
+//  Created by Jiahui Chen  on 4/18/24.
+//
+
+import SwiftUI
+
+@main
+struct BookBeliApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
